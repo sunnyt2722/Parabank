0eaf74ebad2e4223726eba9aa9a1c53c3373ef4f
sunnyt2722 <sunnyt135@gmail.com>
2024-07-21 12:54:06 +0100

    Added script for Account statement

--- src/test/java/sunnykumarlearning/Test/Parabank/PrintAccountStatement.java
+++ src/test/java/sunnykumarlearning/Test/Parabank/PrintAccountStatement.java
@@ -1,44 +1,35 @@
 package sunnykumarlearning.Test.Parabank;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class PrintAccountStatement {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
-		String username="sunnyautotest2";
-		
+
 		driver.get("https://parabank.parasoft.com/parabank/admin.htm");
-		driver.findElement(By.xpath("//a[text()='Register']")).click();
-		driver.findElement(By.id("customer.firstName")).sendKeys("FirstNameSk");
-		driver.findElement(By.id("customer.lastName")).sendKeys("LaastName");
-		driver.findElement(By.id("customer.address.street")).sendKeys("Address");
-		driver.findElement(By.id("customer.address.city")).sendKeys("City");
-		driver.findElement(By.id("customer.address.state")).sendKeys("State");
-		driver.findElement(By.id("customer.address.zipCode")).sendKeys("2345345");
-		driver.findElement(By.id("customer.phoneNumber")).sendKeys("3454643456");
-		driver.findElement(By.id("customer.ssn")).sendKeys("34546");
-		driver.findElement(By.id("customer.username")).sendKeys(username);
-		driver.findElement(By.id("customer.password")).sendKeys("test");
-		driver.findElement(By.id("repeatedPassword")).sendKeys("test");
-	
-		driver.findElement(By.cssSelector("[value='Register']")).click();
-
-		WebElement welcomeMessage = driver.findElement(By.xpath("//h1"));
-		WebElement successMessage = driver.findElement(By.xpath("//div[@id='rightPanel']//p"));
-
-		Assert.assertEquals(welcomeMessage.getText(), "Welcome "+username);
-		Assert.assertEquals(successMessage.getText(), "Your account was created successfully. You are now logged in.");
-
-		System.out.println("welcomeMessage.getText() :: "+welcomeMessage.getText());
-		System.out.println("successMessage.getText() :: "+successMessage.getText());
+		driver.findElement(By.cssSelector("[name='username']")).sendKeys("sunnyautotest2");
+		driver.findElement(By.cssSelector("[name='password']")).sendKeys("test");
+		driver.findElement(By.cssSelector("[value='Log In']")).click();
 		
+		Thread.sleep(5000);
+		
+		List<WebElement> accountDetails = driver.findElements(By.xpath("//table[@id='accountTable']//tbody/tr[1]/td"));
+		WebElement logoutButton = driver.findElement(By.xpath("//a[text()='Log Out']"));
+		
+		for(int i=0;i<accountDetails.size();i++)
+			System.out.println("Value of i is "+i+" : "+accountDetails.get(i).getText());
+		
+		logoutButton.click();
+		
+		driver.quit();
 	}
 }
